21469a6a4538e13b2eeb3625a39490a15a82fe86
Wyatt Roersma <wyatt.roersma@target.com>
2019-02-13 00:31:48 -0700

    add hash reference to each new index parse pe data parse trid data parse mitre data format hash data Tag Release 0.0.0.3

--- ircli.py
+++ ircli.py
@@ -33,13 +33,57 @@ def intel_reaper(plugin, vault_token, vault_url, vault_se, vault_name, input_fil
         if report == "es":
             report_process_time = udatetime.now_to_string()
             processed_json = flatten_dictionary(report_data)
+            md5_hash = report_data["analysis"]["content"]["mainObject"]["hashes"]["md5"]
             for item in processed_json["incidents"]:
-                incidents_dict = flatten_dictionary_with_int(item)
+                incidents_dict = flatten_dictionary(item)
                 incidents_dict["process_time"] = report_process_time
+                incidents_dict["md5_hash"] = md5_hash
+                if "events" in incidents_dict:
+                    for event_items in incidents_dict["events"]:
+                        event_dict = flatten_dictionary(event_items)
+                        event_dict["md5_hash"] = md5_hash
+                        event_dict["process_time"] = report_process_time
+                        index_data_to_es("anyrunevents", payload=event_dict)
+                    del incidents_dict["events"]
                 index_data_to_es("anyrunincidents", payload=incidents_dict)
             del processed_json["incidents"]
+            for item in processed_json["processes"]:
+                processes_dict = flatten_dictionary(item)
+                processes_dict["process_time"] = report_process_time
+                processes_dict["md5_hash"] = md5_hash
+                index_data_to_es("anyrunprocesses", payload=processes_dict)
+            del processed_json["processes"]
+            for item in processed_json["mitre"]:
+                mitre_dict = flatten_dictionary(item)
+                mitre_dict["process_time"] = report_process_time
+                mitre_dict["md5_hash"] = md5_hash
+                index_data_to_es("anyrunmitre", payload=mitre_dict)
+            del processed_json["mitre"]
             processed_json["process_time"] = report_process_time
-            index_data_to_es("anyruntest", payload=processed_json)
+            processed_json["main"] = flatten_dictionary(report_data["analysis"]["content"]["mainObject"]["info"])
+            processed_json["hashes"] = flatten_dictionary(report_data["analysis"]["content"]["mainObject"]["hashes"])
+            processed_json["md5_hash"] = md5_hash
+            del processed_json["report"]["analysis.content.mainObject.hashes"]
+            del processed_json["report"]["analysis.content.mainObject.info"]
+            del processed_json["main"]["trid"]
+            if "pe" in processed_json["main"]:
+                for pe_item in processed_json["main"]["pe"]:
+                    pe_dict = flatten_dictionary_with_int(pe_item)
+                    pe_dict["process_time"] = report_process_time
+                    pe_dict["md5_hash"] = md5_hash
+                    for value_item in processed_json["main"]["pe"]:
+                        pe_dict[str(value_item["key"])] = str(value_item["value"])
+                    del pe_dict["value"]
+                    index_data_to_es("anyrunpe", payload=pe_dict)
+                del processed_json["main"]["pe"]
+            if "trid" in processed_json["main"]:
+                for trid_item in processed_json["main"]["trid"]:
+                    trid_dict = flatten_dictionary(trid_item)
+                    trid_dict["process_time"] = report_process_time
+                    trid_dict["md5_hash"] = md5_hash
+                    index_data_to_es("anyruntrid", payload=trid_dict)
+                del processed_json["main"]["trid"]
+            index_data_to_es("anyrunreport", payload=processed_json)
 
 
 if __name__ == '__main__':
